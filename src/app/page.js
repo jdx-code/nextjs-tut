6994d7dded38ce95aa8f5b1d5d61466a5fad42f5
Jyotirmoy <jyotirmoydas12@gmail.com>
2024-01-16 14:11:33 +0530

    Events, functions and states in NextJS

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,41 @@
+'use client' 
 import Image from 'next/image'
+import { useState } from 'react'
 import styles from './page.module.css'
 
 export default function Home() {
+
+  const [name, setName] = useState("Jyotirmoy")
+
+  // Example showing a function without any parameter
+  const fruit = () => {
+    alert('apple')
+  }
+
+  // Example showing a function with a parameter
+  const color = (item) => {
+    alert(item)
+  }
+
+  // Example showing the use of states
+  const changeName = (item) => {
+    setName(item)
+  }
+
   return (
     <main className={styles.main}>
       
       <HelloComponent />      
-      <HelloComponent name="Jyotirmoy"/>      
+      <HelloComponent name="Jyotirmoy"/>
+
+      <button onClick={()=>alert("Hello..")}> Click me </button>      
+
+      <button onClick={fruit}> Click fruit </button>      
+
+      <button onClick={() => color('blue')}> Click color </button>      
+
+      <p> Name : {name} </p>
+      <button onClick={() => changeName('JD')}> Change name </button>      
       
     </main>
   )
