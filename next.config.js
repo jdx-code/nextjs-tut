ded725295d18529c9438dbbdcf4877aee790f77e
Jyotirmoy <jyotirmoydas12@gmail.com>
2024-01-27 14:36:16 +0530

    Implementing Redirects

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    redirects: async () => {
+        return [
+            {
+                source : '/studentlist',
+                destination : '/login',
+                permanent : false
+            },
+            {
+                source : '/study',
+                destination : '/login',
+                permanent : false
+            },
+        ]
+    }
+}
 
 module.exports = nextConfig
